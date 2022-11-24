35b3fe3af13e52cd671acabdee58656359792ae9
Chepngeno-langat <langatkarenn@gmail.com>
2022-11-24 10:44:59 +0300

    models and tests

--- models/base_model.py
+++ models/base_model.py
@@ -21,4 +21,24 @@ class BaseModel:
                 if key != "__class__":
                     setattr(self, key, value)
 
-    
+    def save(self):
+        """updates updated_at with the current datetime"""
+        self.updated_at = datetime.utcnow()
+
+    def to_dict(self):
+        """Returns a dictionary representation of the BaseModel instance
+        Includes the key/value pair __class__ that represents
+        the class name of the object.
+        """
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop("_sa_instance_state", None)
+        return my_dict
+
+    def __str__(self):
+        """Returns the print/str representation of the BaseModel instance"""
+        dict = self.__dict__.copy()
+        dict.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, dict)
