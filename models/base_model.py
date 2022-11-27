c4fdc50364df9180e274b8c063a58b682a7d46ca
Chepngeno-langat <langatkarenn@gmail.com>
2022-11-27 20:14:48 +0300

    hbnb

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,8 @@
 """Defines the BaseModel class."""
 from uuid import uuid4
 from datetime import datetime
+from models import storage
+
 
 class BaseModel:
     """Defines the BaseModel class"""
@@ -12,18 +14,25 @@ class BaseModel:
             *args(any): Unused
             **kwargs(dict): Key/value pairs of attributes
         """
-        self.id = str(uuid4())
-        self.created_at = self.updated_at = datetime.utcnow()
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.utcnow()
+            storage.new(self)
 
     def save(self):
         """updates updated_at with the current datetime"""
         self.updated_at = datetime.utcnow()
+        storage.save()
 
     def to_dict(self):
         """Returns a dictionary representation of the BaseModel instance
