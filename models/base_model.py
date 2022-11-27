b293487b935c8c83de0e37179bc7ef296904b32d
Chepngeno-langat <langatkarenn@gmail.com>
2022-11-27 22:35:51 +0300

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -14,20 +14,12 @@ class BaseModel:
             *args(any): Unused
             **kwargs(dict): Key/value pairs of attributes
         """
-        if kwargs is not None and kwargs != {}:
-            for key in kwargs:
-                if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
-                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
-                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-                else:
-                    self.__dict__[key] = kwargs[key]
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.utcnow()
-            storage.new(self)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
 
     def save(self):
         """updates updated_at with the current datetime"""
